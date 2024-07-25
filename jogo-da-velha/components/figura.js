8b5ee5f0e86a503addeea301beee88b9e6e15dac
kamilisimionato <kamili.simionato@etec.sp.gov.br>
2024-07-25 12:28:58 -0300

    componente de figura

---
+++ jogo-da-velha/components/figura.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
+
+export default function figura() {
+  return (
+    <View>
+      <Text>figura</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
